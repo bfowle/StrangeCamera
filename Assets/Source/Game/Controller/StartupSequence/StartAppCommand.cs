86220b73f2dfa383bc661e819ad5efc89fb7fb68
bfowle <brettfowle@gmail.com>
2013-10-26 18:42:18 -0400

    Tweaks to project settings before export test #1

--- Assets/Source/Game/Controller/StartupSequence/StartAppCommand.cs
+++ Assets/Source/Game/Controller/StartupSequence/StartAppCommand.cs
@@ -15,18 +15,6 @@ namespace StrangeCamera.Game {
 		public IContext context { get; set; }
 		
 		public override void Execute() {
-			// if we're not the first context, we need to shut down the AudioListener
-			// this is one way to do it, but there is no "right" way
-			if (context != Context.firstContext) {
-				foreach (AudioListener listener in Camera.FindObjectsOfType(
-					typeof(AudioListener)) as AudioListener[]) {
-					listener.enabled = false;
-				}
-			}
-			
-			// MonoBehaviours can only be injected after they've been instantiated manually
-			// here we create the main GameLoop, attaching it to the ContextView.
-			
 			// attach the GameLoop MonoBehaviour to the contextView
 			contextView.AddComponent<GameLoop>();
 			IGameTimer timer = contextView.GetComponent<GameLoop>();
